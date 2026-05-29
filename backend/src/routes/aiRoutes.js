2990e743b785fc2b48c08dfd9cb011d9d719148b
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-29 15:51:17 +0530

    feat: hard coded message removed from backend ai and llama-3.1-8b-instant used

--- backend/src/routes/aiRoutes.js
+++ backend/src/routes/aiRoutes.js
@@ -1,16 +1,8 @@
 import router from 'express';
-
+import { chatWithAI } from '../controllers/chatController.js';
 const aiRoutes = router.Router();
 
 // Example AI chat route
-aiRoutes.post('/', async (req, res) => {
-  const { message } = req.body;
-
-  // Here you would integrate with your AI model to get a response
-  // For demonstration, we'll just echo the message back
-  const aiResponse = "hello you are criticaly ill and you need to go to hospital immediately";
-
-  res.json({ response: aiResponse });
-});
+aiRoutes.post('/', chatWithAI);
 
 export default aiRoutes;
