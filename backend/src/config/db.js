5cf83383f7dd1ac4a3290a7e81c9add27e65a98d
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-31 01:31:59 +0530

    fix: postgres cloud database connection successful

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,23 +1,33 @@
 import pkg from 'pg';
 import dotenv from 'dotenv';
+import dns from 'dns';
 
 dotenv.config();
+
+dns.setDefaultResultOrder('ipv4first'); 
+
 const { Pool } = pkg;
+
+const isLocalhost = process.env.DB_URL.includes('localhost') || process.env.DB_URL.includes('127.0.0.1');
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-})
-
+    ssl: isLocalhost ? false : {
+        rejectUnauthorized: false
+    }
+});
 
 const connectDB = async () => {
     try {
         const client = await pool.connect();
+        console.log('PostgreSQL Connected Successfully to Cloud/Docker!');
         client.release();
     } catch (err) {
-        console.error('Error connecting to PostgreSQL database', err);
-        process.exit(1);
+        console.error('Error connecting to PostgreSQL database:', err.message);
+        setTimeout(() => {
+            process.exit(1);
+        }, 1000);
     }   
 }
-
 
 export { connectDB, pool };
